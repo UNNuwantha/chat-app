1d2c7398161bf6484947fbca01a7546090503c56
UNNuwantha <www.nadun50@gmail.com>
2026-03-25 08:24:35 +0530

    Developed fully Backend

--- server/controllers/messageCntroller.js
+++ server/controllers/messageCntroller.js
@@ -1,5 +1,7 @@
-import Message from "../models/Message";
-import User from "../models/User";
+import Message from "../models/Message.js";
+import User from "../models/User.js";
+import cloudinary from "../lib/cloudinary.js";
+import { io, userSocketMap } from "../server.js";
 
 
 //Get all users expcept the logged in user
@@ -22,4 +24,71 @@ export const getUserForSidedbar = async (req, res)=>{
         console.log(error.message);
         res.json({success: false, message: error.message});
     }
+}
+
+//Getall messages for selected user
+export const getMessages = async (req,res) =>{
+  try {
+    const { id: selectedUserId } = req.params;
+    const myId = req.user._id;
+
+    const messages = await Message.find({
+      $or:[
+        {senderId:myId, receiverId: selectedUserId},
+        {senderId: selectedUserId, receiverId: myId},
+      ]
+    })
+    await Message.updateMany({senderId: selectedUserId, receiverId: myId}, {seen: true});
+    res.json({success: true, messages})
+
+  } catch (error) {
+    console.log(error.message);
+    res.json({success:false, message:error.message})
+  }
+}
+
+//api to mark message as seen using message id
+export const markMessageAsSeen = async(req,res)=>{
+      try {
+        const { id } = req.params;
+       await Message.findByIdAndUpdate(id, {seen: true})
+       res.json({success:true})
+
+      } catch (error) {
+        console.log(error.message);
+        res.json({success:false, message:  error.message})
+      }
+}
+
+//send message to selected user
+export const sendMessage = async (req,res) =>{
+      try {
+        const {text, image} = req.body;
+        const receiverId = req.params.id;
+        const senderId = req.user._id;
+        
+        let imageUrl;
+        if(image){
+           const uploadResponse = await cloudinary.uploader.upload(image)
+           imageUrl = uploadResponse.secure_url;
+        }
+
+        const newMessage = await Message.create({
+          senderId,
+          receiverId,
+          text,
+          image: imageUrl
+        })
+        //Emit the new message to the receiver's socket
+        const receiverSocketId = userSocketMap[receiverId];
+        if(receiverSocketId){
+          io.to(receiverSocketId).emit("newMessage", newMessage)
+        }
+
+        res.json({success: true, newMessage});
+
+      } catch (error) {
+        console.log(error.message);
+        res.json({success: false, message: error.message})
+      }
 }
--- server/server.js
+++ server/server.js
@@ -4,10 +4,36 @@ import cors from "cors";
 import http from "http";
 import { connectDB } from "./lib/db.js";
 import userRouter from "./routes/userRoutes.js";
+import messageRouter from "./routes/messageRoutes.js";
+import { Server } from "socket.io";
 
 //Create Express app and HTTP server
 const app = express();
 const server = http.createServer(app)
+
+//initialize socket.io server
+export const io = new Server(server, {
+    cors: {origin: "*"}
+})
+
+//store online users
+export const userSocketMap = {};//{userId: socketId}
+
+io.on("connection", ()=>{
+    const userId = Socket.handshake.query.userId;
+    console.log("User connected: "+userId);
+
+    if(userId) userSocketMap[userId] = Socket.id;
+
+    //Emit online users to all connected clients
+    io.emit("getOnlineUsers", Object.keys(userSocketMap));
+    Socket.on("disconnect", ()=>{
+        console.log("User disconnected: ", userId);
+        delete userSocketMap[userId];
+        io.emit("getOnlineUsers", Object.keys(userSocketMap));
+    })
+
+})
 
 //Middleware setup
 app.use(express.json({limit: "4mb"}));
@@ -15,9 +41,10 @@ app.use(cors());
 
 app.use("/api/status", (req, res)=> res.send("Server is live") );
 app.use("/api/auth", userRouter);
+app.use("/api/messages", messageRouter);
 
 //Connect to MongoDB
 await connectDB();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 server.listen(PORT, ()=> console.log("Server is running on PORT: "+PORT));
